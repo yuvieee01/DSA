c8c0e37ee3bee0e24416d6d5a9ff3a4f2fe177c9
Yuvraj <139042073+yuvieee01@users.noreply.github.com>
2026-03-26 11:48:54 +0530

    Update solution: 242. Valid Anagram

--- 242. Valid Anagram/242. Valid Anagram.py
+++ 242. Valid Anagram/242. Valid Anagram.py
@@ -1,24 +1,25 @@
 #
 # Problem: 242. Valid Anagram
 # Difficulty: Easy
-# Link: https://leetcode.com/problems/valid-anagram/description/
+# Link: https://leetcode.com/problems/valid-anagram/
 # Language: python3
 # Date: 2026-03-26
 
 
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        d = {}
-        for i in s:
-            if i in d:
-                d[i] += 1
-            else:
-                d[i] = 1
-        for i in t:
-            if i not in d:
+        if len(s) != len(t):
+            return False
+
+        count = [0] * 26
+
+        for ch in s:
+            count[ord(ch) - ord('a')] += 1
+
+        for ch in t:
+            idx = ord(ch) - ord('a')
+            count[idx] -= 1
+            if count[idx] < 0:
                 return False
-            else:
-                if d[i] == 0:
-                    return False
-                d[i]-=1
+
         return True
