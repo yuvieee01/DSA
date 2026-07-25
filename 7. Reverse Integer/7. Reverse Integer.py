daa5b5bc9d9c33ce88c6e51172e65c13052c4e4a
Yuvraj <139042073+yuvieee01@users.noreply.github.com>
2026-07-25 19:26:27 +0530

    Add solution: 7. Reverse Integer

---
+++ 7. Reverse Integer/7. Reverse Integer.py
@@ -0,0 +1,18 @@
+#
+# Problem: 7. Reverse Integer
+# Difficulty: Medium
+# Link: https://leetcode.com/problems/reverse-integer/
+# Language: python3
+# Date: 2026-07-25
+
+
+class Solution:
+    def reverse(self, x: int) -> int:
+        if x < 0:
+            x = int(f"-{(str(x)[::-1])[:len(str(x))-1]}")
+        else:
+            x = int(f"{str(x)[::-1]}")
+
+        if x > (2**31)-1 or x < -2**31:
+            return 0
+        else: return x
