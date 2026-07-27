08c3e0e5c25c5b7ba57eb16222e49846c07fbb67
Yuvraj <139042073+yuvieee01@users.noreply.github.com>
2026-07-27 11:26:05 +0530

    Update solution: 189. Rotate Array

--- 189. Rotate Array/189. Rotate Array.py
+++ 189. Rotate Array/189. Rotate Array.py
@@ -1,12 +1,26 @@
 #
 # Problem: 189. Rotate Array
 # Difficulty: Medium
-# Link: https://leetcode.com/problems/rotate-array/
+# Link: https://leetcode.com/problems/rotate-array/submissions/2082888849/
 # Language: python3
 # Date: 2026-07-27
 
 
-# What I did (Pythonic Way):-
+# What i optimised (The Better Approach (Extra Memory))
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        n = len(nums)
+        k = k % n
+        res = [0] * n   # Extra Memory
+
+        for i in range(n):
+            res[(i+k) % n] = nums[i]    # new index: (i+k)%n
+
+        nums[:] = res
+
+
+"""
+# What I did (Brute)
 class Solution:
     def rotate(self, nums: List[int], k: int) -> None:
         n = len(nums)
@@ -17,3 +31,4 @@ class Solution:
                 nums[i+1] = nums[i]
             nums[0] = temp
             k -= 1
+"""
