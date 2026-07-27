6cef6c2f887bf7bf41849d52e5147e53817c53dc
Yuvraj <139042073+yuvieee01@users.noreply.github.com>
2026-07-27 11:00:44 +0530

    Update solution: 189. Rotate Array

--- 189. Rotate Array/189. Rotate Array.py
+++ 189. Rotate Array/189. Rotate Array.py
@@ -6,15 +6,14 @@
 # Date: 2026-07-27
 
 
-# What I did (Brute)
+# What I did (Pythonic Way):-
 class Solution:
     def rotate(self, nums: List[int], k: int) -> None:
-        """
-        Do not return anything, modify nums in-place instead.
-        """
+        n = len(nums)
+        k = k % n   # If k > length of nums
         while k != 0:
             temp = nums[-1]
-            for i in range(len(nums)-2, -1, -1):
+            for i in range(n-2, -1, -1):
                 nums[i+1] = nums[i]
             nums[0] = temp
             k -= 1
