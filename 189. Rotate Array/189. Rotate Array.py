7f0ce9659763dd9de7dfc9623009ae35098a2cd1
Yuvraj <139042073+yuvieee01@users.noreply.github.com>
2026-07-27 12:12:23 +0530

    Update solution: 189. Rotate Array

--- 189. Rotate Array/189. Rotate Array.py
+++ 189. Rotate Array/189. Rotate Array.py
@@ -1,23 +1,29 @@
 #
 # Problem: 189. Rotate Array
 # Difficulty: Medium
-# Link: https://leetcode.com/problems/rotate-array/submissions/2082888849/
+# Link: https://leetcode.com/problems/rotate-array/submissions/2082952985/
 # Language: python3
 # Date: 2026-07-27
 
 
-# What i optimised (The Better Approach (Extra Memory))
 class Solution:
+    def rev(self, arr, l, r):
+        while l < r:
+            arr[l], arr[r] = arr[r], arr[l]
+            l+=1
+            r-=1
+
     def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
         n = len(nums)
         k = k % n
-        res = [0] * n   # Extra Memory
 
-        for i in range(n):
-            res[(i+k) % n] = nums[i]    # new index: (i+k)%n
-
-        nums[:] = res
-
+        self.rev(nums, 0, n-1)
+        self.rev(nums, 0, k-1)
+        self.rev(nums, k, n-1)
+        
 
 """
 # What I did (Brute)
@@ -31,4 +37,18 @@ class Solution:
                 nums[i+1] = nums[i]
             nums[0] = temp
             k -= 1
+
+===========================================================
+
+# What i optimised (The Better Approach (Extra Memory))
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        n = len(nums)
+        k = k % n
+        res = [0] * n   # Extra Memory
+
+        for i in range(n):
+            res[(i+k) % n] = nums[i]    # new index: (i+k)%n
+
+        nums[:] = res
 """
