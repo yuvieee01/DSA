6a6aae6dcc0d6e1ca5698c63922f86f2d2ed8891
Yuvraj <139042073+yuvieee01@users.noreply.github.com>
2026-07-29 23:40:30 +0530

    Update solution: 121. Best Time to Buy and Sell Stock

--- 121. Best Time to Buy and Sell Stock/121. Best Time to Buy and Sell Stock.py
+++ 121. Best Time to Buy and Sell Stock/121. Best Time to Buy and Sell Stock.py
@@ -1,7 +1,7 @@
 #
 # Problem: 121. Best Time to Buy and Sell Stock
 # Difficulty: Easy
-# Link: https://leetcode.com/problems/best-time-to-buy-and-sell-stock/description/
+# Link: https://leetcode.com/problems/best-time-to-buy-and-sell-stock/submissions/2086560997/
 # Language: python3
 # Date: 2026-07-29
 
@@ -9,11 +9,10 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         pro = 0
-        m = prices[0]
+        ch = prices[0]
 
-        for i in range(1,len(prices)):
-            curr = prices[i] - m
-            pro = max(curr, pro)
-            m = min(prices[i], m)
+        for i in range(len(prices)):
+            ch = min(ch, prices[i])
+            pro = max(pro, prices[i]-ch)
         
         return pro
