63008a66dbbf3c9c8199f0b381713f07da7cf2c2
Yuvraj <139042073+yuvieee01@users.noreply.github.com>
2026-08-04 00:54:25 +0530

    Update solution: 1464. Maximum Product of Two Elements in an Array

--- 1464. Maximum Product of Two Elements in an Array/1464. Maximum Product of Two Elements in an Array.py
+++ 1464. Maximum Product of Two Elements in an Array/1464. Maximum Product of Two Elements in an Array.py
@@ -1,13 +1,30 @@
 #
 # Problem: 1464. Maximum Product of Two Elements in an Array
 # Difficulty: Easy
-# Link: https://leetcode.com/problems/maximum-product-of-two-elements-in-an-array/submissions/2093139931/
+# Link: https://leetcode.com/problems/maximum-product-of-two-elements-in-an-array/submissions/2093148626/
 # Language: python3
 # Date: 2026-08-03
 
 
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        max1 = float('-inf')
+        max2 = float('-inf')
+
+        for num in nums:
+            if num > max1:
+                max2 = max1     # If we find a new biggest number, the old biggest gets demoted to second place
+                max1 = num
+
+            elif num > max2:
+                max2 = num
+        
+        return (max1 - 1) * (max2 - 1)
+
+'''
 # What I did: TC - O(N log N)
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
         nums.sort()     # Sorts the list in-place
         return ((nums[-1]-1)*(nums[-2]-1))
+'''
