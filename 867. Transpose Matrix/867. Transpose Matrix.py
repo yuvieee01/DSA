8ed39c39581777e698b4ee0d54a2f4961d850451
Yuvraj <139042073+yuvieee01@users.noreply.github.com>
2026-08-06 23:50:37 +0530

    Update solution: 867. Transpose Matrix

--- 867. Transpose Matrix/867. Transpose Matrix.py
+++ 867. Transpose Matrix/867. Transpose Matrix.py
@@ -18,3 +18,9 @@ class Solution:
                 res[j][i] = matrix[i][j]
         
         return res
+
+'''
+don't create Blank 2D matrix canvas like:
+    res = [[0] * rows] * cols
+because, in python, It looks completely logical, but it creates a massive bug. When you use the * operator to multiply a list containing another list, Python does not create independent copies of the inner list. Instead, it creates multiple references to the exact same list in memory.
+'''
