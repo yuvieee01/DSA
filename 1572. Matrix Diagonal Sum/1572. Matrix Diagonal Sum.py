7b96009ff378bc24475635b89430cf3351692d59
Yuvraj <139042073+yuvieee01@users.noreply.github.com>
2026-08-06 22:05:57 +0530

    Add solution: 1572. Matrix Diagonal Sum

---
+++ 1572. Matrix Diagonal Sum/1572. Matrix Diagonal Sum.py
@@ -0,0 +1,19 @@
+#
+# Problem: 1572. Matrix Diagonal Sum
+# Difficulty: Easy
+# Link: https://leetcode.com/problems/matrix-diagonal-sum/submissions/2096971531/
+# Language: python3
+# Date: 2026-08-06
+
+
+class Solution:
+    def diagonalSum(self, mat: List[List[int]]) -> int:
+        n = len(mat)
+        s = 0
+        for i in range(n):
+            s += mat[i][i]
+            
+            if i != n - 1 - i:
+                s += mat[i][n - 1 - i]
+        
+        return s
