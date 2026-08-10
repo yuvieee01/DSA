e7d5636744ab362c7fada6bb1dfaced9b4c6a261
Yuvraj <139042073+yuvieee01@users.noreply.github.com>
2026-08-10 21:52:56 +0530

    Update solution: 268. Missing Number

--- 268. Missing Number/268. Missing Number.py
+++ 268. Missing Number/268. Missing Number.py
@@ -1,9 +1,9 @@
 #
 # Problem: 268. Missing Number
 # Difficulty: Easy
-# Link: https://leetcode.com/problems/missing-number/description/
+# Link: https://leetcode.com/problems/missing-number/submissions/2100606116/
 # Language: python3
-# Date: 2026-08-09
+# Date: 2026-08-10
 
 
 from collections import defaultdict
