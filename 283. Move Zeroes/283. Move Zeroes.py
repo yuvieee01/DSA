0c2c87e03abb226809a87b57b26672f428712671
Yuvraj <139042073+yuvieee01@users.noreply.github.com>
2026-08-10 21:53:25 +0530

    Update solution: 283. Move Zeroes

--- 283. Move Zeroes/283. Move Zeroes.py
+++ 283. Move Zeroes/283. Move Zeroes.py
@@ -1,9 +1,9 @@
 #
 # Problem: 283. Move Zeroes
 # Difficulty: Easy
-# Link: https://leetcode.com/problems/move-zeroes/description/
+# Link: https://leetcode.com/problems/move-zeroes/
 # Language: python3
-# Date: 2026-08-09
+# Date: 2026-08-10
 
 
 class Solution:
