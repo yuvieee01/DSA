1ae7587c4875e7eb3eeb40df9472333239257b76
Yuvraj <139042073+yuvieee01@users.noreply.github.com>
2026-08-10 21:53:47 +0530

    Update solution: 2149. Rearrange Array Elements by Sign

--- 2149. Rearrange Array Elements by Sign/2149. Rearrange Array Elements by Sign.py
+++ 2149. Rearrange Array Elements by Sign/2149. Rearrange Array Elements by Sign.py
@@ -1,9 +1,9 @@
 #
 # Problem: 2149. Rearrange Array Elements by Sign
 # Difficulty: Medium
-# Link: https://leetcode.com/problems/rearrange-array-elements-by-sign/description/
+# Link: https://leetcode.com/problems/rearrange-array-elements-by-sign/
 # Language: python3
-# Date: 2026-08-09
+# Date: 2026-08-10
 
 
 # Optimal Approach:
