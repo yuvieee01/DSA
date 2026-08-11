5068be49cc36bb816e88dee43caea1d6d507881d
Yuvraj <139042073+yuvieee01@users.noreply.github.com>
2026-08-11 22:13:50 +0530

    Update solution: 485. Max Consecutive Ones

--- 485. Max Consecutive Ones/485. Max Consecutive Ones.py
+++ 485. Max Consecutive Ones/485. Max Consecutive Ones.py
@@ -1,9 +1,9 @@
 #
 # Problem: 485. Max Consecutive Ones
 # Difficulty: Easy
-# Link: https://leetcode.com/problems/max-consecutive-ones/submissions/2100606374/
+# Link: https://leetcode.com/problems/max-consecutive-ones/
 # Language: python3
-# Date: 2026-08-10
+# Date: 2026-08-11
 
 
 class Solution:
