e052bd97e881625458c6ffaa449915958c733251
Yuvraj <139042073+yuvieee01@users.noreply.github.com>
2026-08-12 22:03:59 +0530

    Update solution: 867. Transpose Matrix

--- 867. Transpose Matrix/867. Transpose Matrix.py
+++ 867. Transpose Matrix/867. Transpose Matrix.py
@@ -1,9 +1,9 @@
 #
 # Problem: 867. Transpose Matrix
 # Difficulty: Easy
-# Link: https://leetcode.com/problems/transpose-matrix/
+# Link: https://leetcode.com/problems/transpose-matrix/description/
 # Language: python3
-# Date: 2026-08-11
+# Date: 2026-08-12
 
 
 class Solution:
