f1fd57951381399374e9f9a930d01fecf3a85c2d
Yuvraj <139042073+yuvieee01@users.noreply.github.com>
2026-08-16 23:05:44 +0530

    Update solution: 867. Transpose Matrix

--- 867. Transpose Matrix/867. Transpose Matrix.py
+++ 867. Transpose Matrix/867. Transpose Matrix.py
@@ -3,9 +3,24 @@
 # Difficulty: Easy
 # Link: https://leetcode.com/problems/transpose-matrix/description/
 # Language: python3
-# Date: 2026-08-15
+# Date: 2026-08-16
 
 
 class Solution:
     def transpose(self, matrix: List[List[int]]) -> List[List[int]]:
+        rows = len(matrix)
+        cols = len(matrix[0])
+
+        res = [[0] * rows for _ in range(cols)]     # Blank 2D matrix canvas..
+
+        for i in range(rows):
+            for j in range(cols):
+                res[j][i] = matrix[i][j]
         
+        return res
+
+'''
+don't create Blank 2D matrix canvas like:
+    res = [[0] * rows] * cols
+because, in python, It looks completely logical, but it creates a massive bug. When you use the * operator to multiply a list containing another list, Python does not create independent copies of the inner list. Instead, it creates multiple references to the exact same list in memory.
+'''
