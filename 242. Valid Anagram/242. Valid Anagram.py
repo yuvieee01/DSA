ddcead15a54c7135c725697ef92de23598491784
Yuvraj <139042073+yuvieee01@users.noreply.github.com>
2026-08-24 21:39:57 +0530

    Update solution: 242. Valid Anagram

--- 242. Valid Anagram/242. Valid Anagram.py
+++ 242. Valid Anagram/242. Valid Anagram.py
@@ -3,9 +3,32 @@
 # Difficulty: Easy
 # Link: https://leetcode.com/problems/valid-anagram/
 # Language: python3
-# Date: 2026-03-26
+# Date: 2026-08-24
 
 
+# Optimal:
+from collections import defaultdict
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        # Map frequency tuples to lists of anagrams
+        anagram_map = defaultdict(list)
+        
+        for word in strs:
+            count = [0] * 26
+            
+            # Build the 26-slot signature for the current word
+            for char in word:
+                count[ord(char) - ord('a')] += 1
+                
+            # Tuples are immutable, making them valid dictionary keys
+            anagram_map[tuple(count)].append(word)
+            
+        # Return all the grouped lists
+        return list(anagram_map.values())
+
+# What i did:
+'''
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         if len(s) != len(t):
@@ -23,3 +46,4 @@ class Solution:
                 return False
 
         return True
+'''
