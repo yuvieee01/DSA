cfe0e9338d10ef867e721debba0a170c21ac54d4
Yuvraj <139042073+yuvieee01@users.noreply.github.com>
2026-09-02 20:17:07 +0530

    Update solution: 796. Rotate String

--- 796. Rotate String/796. Rotate String.py
+++ 796. Rotate String/796. Rotate String.py
@@ -6,7 +6,36 @@
 # Date: 2026-09-02
 
 
+# Optimal:
+class Solution:
+    def rotateString(self, s: str, goal: str) -> bool:
+        # Guardrail: They must be the exact same length
+        if len(s) != len(goal):
+            return False
+            
+        doubled_s = s + s
+        n = len(s)
+        
+        # Slide a window across doubled_s
+        # We only need to check 'n' starting positions
+        for i in range(n):
+            is_match = True
+            
+            # Manually check character by character
+            for j in range(n):
+                # If any character doesn't match, this starting position is wrong
+                if doubled_s[i + j] != goal[j]:
+                    is_match = False
+                    break
+            
+            # If we made it through the whole inner loop without breaking, we found it!
+            if is_match:
+                return True
+                
+        return False
+
 # Pythonic way:
+'''
 # Concatenated approach (s+s):
 class Solution:
     def rotateString(self, s: str, goal: str) -> bool:
@@ -17,7 +46,6 @@ class Solution:
 
         return new_s.find(goal) != -1
 
-'''
 class Solution:
     def rotateString(self, s: str, goal: str) -> bool:
         if len(s) != len(goal):
