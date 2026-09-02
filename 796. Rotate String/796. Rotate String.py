c0b05af47984aec676cedd361433f39a30045371
Yuvraj <139042073+yuvieee01@users.noreply.github.com>
2026-09-02 19:42:25 +0530

    Update solution: 796. Rotate String

--- 796. Rotate String/796. Rotate String.py
+++ 796. Rotate String/796. Rotate String.py
@@ -7,6 +7,17 @@
 
 
 # Pythonic way:
+# Concatenated approach (s+s):
+class Solution:
+    def rotateString(self, s: str, goal: str) -> bool:
+        if len(s) != len(goal):
+            return False
+
+        new_s = s + s
+
+        return new_s.find(goal) != -1
+
+'''
 class Solution:
     def rotateString(self, s: str, goal: str) -> bool:
         if len(s) != len(goal):
@@ -18,7 +29,7 @@ class Solution:
             return True
         else:
             return False
-
+'''
 
 # Brute:
 '''
