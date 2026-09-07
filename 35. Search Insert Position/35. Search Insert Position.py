488ffaf42ab5680bce5319c0e9a03863f0f4b9ee
Yuvraj <139042073+yuvieee01@users.noreply.github.com>
2026-09-07 23:27:03 +0530

    Add solution: 35. Search Insert Position

---
+++ 35. Search Insert Position/35. Search Insert Position.py
@@ -0,0 +1,24 @@
+#
+# Problem: 35. Search Insert Position
+# Difficulty: Easy
+# Link: https://leetcode.com/problems/search-insert-position/description/
+# Language: python3
+# Date: 2026-09-07
+
+
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums) - 1  # This is a binary search question. O(log N)
+
+        while l <= r:
+            mid = (l + r) // 2
+
+            if nums[mid] < target:
+                l = mid + 1
+            elif nums[mid] > target:
+                r = mid - 1
+            else:
+                return mid
+
+        return l
+
