df565659cc9801c4d0da4eb826cb7bab0eec884d
Yuvraj <139042073+yuvieee01@users.noreply.github.com>
2026-09-13 20:13:36 +0530

    Add solution: 540. Single Element in a Sorted Array

---
+++ 540. Single Element in a Sorted Array/540. Single Element in a Sorted Array.py
@@ -0,0 +1,23 @@
+#
+# Problem: 540. Single Element in a Sorted Array
+# Difficulty: Medium
+# Link: https://leetcode.com/problems/single-element-in-a-sorted-array/
+# Language: python3
+# Date: 2026-09-13
+
+
+class Solution:
+    def singleNonDuplicate(self, nums: List[int]) -> int:
+        left, right = 0, len(nums) - 1
+
+        while left < right:
+            mid = left + (right - left) // 2
+            mid = (mid // 2) * 2    # mid = mid - (mid % 2) OR mid = mid ^ 1
+            
+            if nums[mid] == nums[mid + 1]:
+                left = mid + 2
+            
+            else:
+                right = mid
+            
+        return nums[left]
