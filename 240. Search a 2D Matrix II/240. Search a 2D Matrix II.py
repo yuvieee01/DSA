0599c7e5586918553ed57c93e2a7253dd893593b
Yuvraj <139042073+yuvieee01@users.noreply.github.com>
2026-09-14 22:22:51 +0530

    Add solution: 240. Search a 2D Matrix II

---
+++ 240. Search a 2D Matrix II/240. Search a 2D Matrix II.py
@@ -0,0 +1,25 @@
+#
+# Problem: 240. Search a 2D Matrix II
+# Difficulty: Medium
+# Link: https://leetcode.com/problems/search-a-2d-matrix-ii/
+# Language: python3
+# Date: 2026-09-14
+
+
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        rows = len(matrix)
+        cols = len(matrix[0])
+
+        row, col = 0, cols - 1
+
+        while row < rows and col >= 0:
+            if matrix[row][col] == target:
+                return True
+            
+            if matrix[row][col] > target:
+                col -= 1
+            else:
+                row += 1
+
+        return False
