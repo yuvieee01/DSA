59dfcb7716072250b68dfe7c6b6c5a710b9290ee
Yuvraj <139042073+yuvieee01@users.noreply.github.com>
2026-09-15 23:26:27 +0530

    Update solution: 240. Search a 2D Matrix II

--- 240. Search a 2D Matrix II/240. Search a 2D Matrix II.py
+++ 240. Search a 2D Matrix II/240. Search a 2D Matrix II.py
@@ -1,9 +1,9 @@
 #
 # Problem: 240. Search a 2D Matrix II
 # Difficulty: Medium
-# Link: https://leetcode.com/problems/search-a-2d-matrix-ii/
+# Link: https://leetcode.com/problems/search-a-2d-matrix-ii/description/
 # Language: python3
-# Date: 2026-09-14
+# Date: 2026-09-15
 
 
 class Solution:
